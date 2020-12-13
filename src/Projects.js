c334778a2ce5fbdf27e081b2e032c3bf7caf5d0d
Zach LaFleur <zach.lafleur95@gmail.com>
2020-12-13 13:34:02 -0700

    created data to be passed through the index.js in projects.js

---
+++ src/Projects.js
@@ -0,0 +1,37 @@
+export const Projects = [
+    {
+        "id": "1",
+        "project": "Streaming Guide",
+        "about": "",
+        "gitHub": "https://github.com/MrCartree/Ultimate-Streaming-Guide",
+        "deployed": "https://imbingz.github.io/Ultimate-Streaming-Guide/"
+    },
+    {
+        "id": "2",
+        "project": "AmalgaNation",
+        "about": "",
+        "gitHub": "https://github.com/SydneyGoodwill/amalganation",
+        "deployed": "https://fast-woodland-71053.herokuapp.com/login"
+    },
+    {
+        "id": "3",
+        "project": "Daily Planner",
+        "about": "",
+        "gitHub": "https://github.com/MrCartree/daily-planner",
+        "deployed": "https://mrcartree.github.io/daily-planner/"
+    },
+    {
+        "id": "4",
+        "project": "Employee Directory",
+        "about": "",
+        "gitHub": "https://github.com/MrCartree/Employee-Directory",
+        "deployed": "https://mrcartree.github.io/Employee-Directory/"
+    },
+    {
+        "id": "5",
+        "project": "Budget Tracker",
+        "about": "",
+        "gitHub": "https://github.com/MrCartree/budget-tracker",
+        "deployed": "https://still-journey-37573.herokuapp.com/"
+    }
+]
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Projects } from "./Projects";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App projects={Projects} />,
   document.getElementById('root')
 );
 
