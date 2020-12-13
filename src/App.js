62867443e8bf0ba9d2f701cb11e3c5c44dfb29f9
Zach LaFleur <zach.lafleur95@gmail.com>
2020-12-13 11:40:56 -0700

    created components, installed rrd, created pages as well.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./component/Navbar/Navbar";
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />     
+        <Header />   
+          {/* <Route exact path="/" component={} />
+          <Route exact path="/" component={} />
+          <Route exact path="/" component={} />
+          <Route exact path="/" component={} />         */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
 export default App;
