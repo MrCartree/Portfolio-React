ccab1be3dab621fb15701dd53e2f852911e070a8
Zach LaFleur <zach.lafleur95@gmail.com>
2020-12-15 11:36:09 -0700

    added text for both the contact page and the homepage.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,9 +8,8 @@ function Contact() {
             <div className="contactCard">
                 <div className="card">
                     <div className="card-body">
-                        <h5 className="card-title">Contact Me</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">Contact Me and stuff</h6>
-                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, necessitatibus voluptates nesciunt explicabo numquam autem totam facilis modi voluptatem rem laborum nihil ratione aut perspiciatis non quisquam quaerat eveniet eum sunt vel impedit dicta assumenda? Nemo eius, placeat fugiat a eligendi blanditiis provident culpa adipisci distinctio facere ipsa, totam rem?</p>
+                        <h5 className="card-title">Lets Get in Touch</h5>
+                        <p className="card-text">You can feel free to contact me here on this page! You can find my Email, GitHub account, and Linkedin profile by clicking one of the images below! I am always looking for new ideas and new projects to complete.</p>
 
                         <div className="row">
                             <div className="column">
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,13 +4,13 @@ import Header from "../component/Header/Header"
 function Homepage() {
     return (
         <React.Fragment>
-            <Header page="About" />
+            <Header page="About Me" />
             <div className="aboutPage" >
                 <div className="card">
                     <div className="card-body">
-                        <h5 className="card-title">About Me</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">Zach LaFleur</h6>
-                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, necessitatibus voluptates nesciunt explicabo numquam autem totam facilis modi voluptatem rem laborum nihil ratione aut perspiciatis non quisquam quaerat eveniet eum sunt vel impedit dicta assumenda? Nemo eius, placeat fugiat a eligendi blanditiis provident culpa adipisci distinctio facere ipsa, totam rem?</p>
+                        <h5 className="card-title">Zach LaFleur</h5>
+                        <p className="card-text">Hello, my name is Zach LaFleur and I am an aspiring web developer. I have made it through a 6 month bootcamp for computer programming and web development at the University of Utah run by Trilogy. My skills include: HTML, CSS, JS, NODEJS, React and so much more.</p>
+                        <p className="card-text">A bit about me, I like to cook things from scratch for my friends and family, I like to think I am a beer enthusiast and always try to pair the best drink with the meal, but mainly I just love creation. The idea and ability to create things from either my own mind or that of an employer is really what drives me to do what I do.</p>
                         <div className="myImageContainer" >
                             <img className="myImage" src="/images/Me.jpg" alt="Zach LaFleur" />
                         </div>
