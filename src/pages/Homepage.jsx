e294c5501977b750b98d9e211e49386f02d4bdc3
Zach LaFleur <zach.lafleur95@gmail.com>
2020-12-15 09:06:36 -0700

    added an image of myself to the project. rendered it on homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,6 +11,9 @@ function Homepage() {
                         <h5 className="card-title">About Me</h5>
                         <h6 className="card-subtitle mb-2 text-muted">Zach LaFleur</h6>
                         <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, necessitatibus voluptates nesciunt explicabo numquam autem totam facilis modi voluptatem rem laborum nihil ratione aut perspiciatis non quisquam quaerat eveniet eum sunt vel impedit dicta assumenda? Nemo eius, placeat fugiat a eligendi blanditiis provident culpa adipisci distinctio facere ipsa, totam rem?</p>
+                        <div className="myImageContainer" >
+                            <img className="myImage" src="/images/Me.jpg" alt="Zach LaFleur" />
+                        </div>
                     </div>
                 </div>
             </div>
