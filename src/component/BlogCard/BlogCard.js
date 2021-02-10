a188957e2718aa60bc0aecae5b228a5d5a86fc6a
Zach LaFleur <zach.lafleur95@gmail.com>
2021-02-10 09:06:46 -0700

    blog post

--- src/component/BlogCard/BlogCard.js
+++ src/component/BlogCard/BlogCard.js
@@ -3,7 +3,7 @@ import "./BlogCard.css";
 
 function ProjectCard(props) {
     return (
-        <div className="card" >
+        <div className="blog-card card" >
             <h3 className="card-text">{props.title}</h3>
             <p>{props.date}</p>
             <div className="card-body">
