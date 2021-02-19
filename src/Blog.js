37b6ee9423f38cd72ad4ba0f0ef75f1a88359784
Zach LaFleur <zach.lafleur95@gmail.com>
2021-02-19 11:13:39 -0700

    added blog post

--- src/Blog.js
+++ src/Blog.js
@@ -1,14 +1,20 @@
 export const Blog = [
     {
-        "id": "1",
-        "title": "Keep Writting",
-        "date": "2/09/2021",
-        "bloggy": "Hello, and welcome to the first blog post. Here I will be posting about things that I learn and am currently putting into practice. I plan to be picking up some books about programming and coding. The goal of this blog portion is to show future employers that I am continually learning as well as helping myself track my progress. I will date each of these posts and updates to show that I am continually updating my portfolio and continuing the learn. Recently I have been working on a project with my brother called Keep Writting. I will supply a link here in this posting and on the portfolio section of this site when we deploy later this month. I have enjoyed working on Keep Writting because I have been able to learn a bit about Ruby and Ruby on Rails and figure out a whole new language. It has been a lot of fun pair programming and working on this project with my brother. It has been very helpful to have someone there ready to help when needed as well as push me into learning."
+        "id": "3",
+        "title": "Algorithms",
+        "date": "2/19/21",
+        "bloggy": "Learning algorithms can be tricky, especially when all you have is a senior in colleges book on algorithms. I started trying to parce through it but so much of it went over my head! What the heck is big O notation? Why have I never heard of it before? Well, after a small tutor session with my brother, he was able to tell me about algorithms. Its what I call the begginers crash course of algorithms, going into BFS (Brute Force Search) as well as BS (Binary Search). I found it all really interesting and I believe it put me on the right course to better understand algorithms and how to solve them.The best part of the tutor session for me, was looking at all the different types of sorting algorithms and learning which ones are best used and where. If you need help understanding how algorithms sort, there is a fantastic resource on toptal.com I highly reccomend you take a look if you have the time.",
     },
     {
         "id": "2",
         "title": "The Job Hunt",
         "date": "2/10/2021",
         "bloggy": "The job hunt continues. Through all the emails, applications, networking that I have done. It really seems scarce, the ability to find someone willing to hire me. While yes, I am bummed out that I havent gotten a job in the industry I am interested in, I have made the time to learn more within that. I am managing to continue to code, or at least study code and other websites to see what works and what doesnt work. The more I look at other sites it makes me think about what more I can do with my own site, make more clickables? Hover effects? Some flashy animation? Instead, I opted for the blog portion to my site. To let future employers know I am still learning."
+    },
+    {
+        "id": "1",
+        "title": "Keep Writting",
+        "date": "2/09/2021",
+        "bloggy": "Hello, and welcome to the first blog post. Here I will be posting about things that I learn and am currently putting into practice. I plan to be picking up some books about programming and coding. The goal of this blog portion is to show future employers that I am continually learning as well as helping myself track my progress. I will date each of these posts and updates to show that I am continually updating my portfolio and continuing the learn. Recently I have been working on a project with my brother called Keep Writting. I will supply a link here in this posting and on the portfolio section of this site when we deploy later this month. I have enjoyed working on Keep Writting because I have been able to learn a bit about Ruby and Ruby on Rails and figure out a whole new language. It has been a lot of fun pair programming and working on this project with my brother. It has been very helpful to have someone there ready to help when needed as well as push me into learning."
     }
 ]
